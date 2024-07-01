bda90a4ce0ff3b08f07e1cbce0a166d4d57419f0
asibulhasanshanto <asibulhasan.rucse.18@gmail.com>
2024-07-02 02:18:11 +0600

    about us page done

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -6,7 +6,7 @@
             </a>
             <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">About Us</a>
+                    <a href="{{url('/about')}}" class="hover:underline me-4 md:me-6">About Us</a>
                 </li>
                 <li>
                     <a href="#" class="hover:underline me-4 md:me-6">Blogs</a>
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,9 +1,9 @@
-<div  id="navbar" class="nav flex items-center justify-center">
+<div  id="navbar" class="nav flex items-center justify-center z-10">
     <div class=" w-[90%] relative bg-[#faf3e6] h-[90px] rounded-bl-xl rounded-br-xl flex items-center justify-between px-[20px]">
         <!-- menu buttons -->
         <div class="menu-buttons flex items-center gap-3">
             <a href="/" class="menu-button ">Products</a>
-            <a href="/" class="menu-button">About Us</a>
+            <a href="{{url('/about')}}" class="menu-button">About Us</a>
 
         </div>
         <!-- logo -->
