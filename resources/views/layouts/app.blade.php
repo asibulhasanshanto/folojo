74a335bb37d764993cb209bfcdc61103027e93f8
asibulhasanshanto <asibulhasan.rucse.18@gmail.com>
2024-07-09 22:46:14 +0600

    product details js added'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/admin/sidebar.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/admin/sidebar.js', 'resources/js/productDetails.js'])
 </head>
 
 <body class="font-sans antialiased">
